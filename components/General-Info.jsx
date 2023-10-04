b9de495e4a7ecc5d55e4e6795f5fd4e09383a04f
Greg Johnston <gregmj.sco@gmail.com>
2023-10-04 21:55:44 +0900

    Add jsx to general info functions and add to root

--- components/General-Info.jsx
+++ components/General-Info.jsx
@@ -1,22 +1,45 @@
 function Name(){
-    return
+    return (
+        <>
+            <label htmlFor="name">
+                Name:
+            </label>
+            <input type="text" id="name" name="name" />
+        </>
+    );
 
 }
 
 function Email(){
-    return
+    return (
+        <>
+            <label htmlFor="email">
+                Email:
+            </label>
+            <input type="email" name="email" id="email" />
+        </>
+    );
 
 }
 
 function Phone(){
-    return
+    return (
+        <>
+            <label htmlFor="phone">
+                Phone:
+            </label>
+            <input type="tel" name="phone" id="phone" />
+        </>
+    );
 
 }
 
 export default function GeneralInfo() {
-    return {
-        <Name />
-        <Email />
-        <Phone />
-    }
+    return (
+        <form>
+            <Name />
+            <Email />
+            <Phone />
+        </form>
+    );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import GeneralInfo from '../components/General-Info.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GeneralInfo />
   </React.StrictMode>,
 )
