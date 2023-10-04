312c1ddf9c3d5a49d51609afd4cb141de66bd5b6
Greg Johnston <gregmj.sco@gmail.com>
2023-10-04 21:44:48 +0900

    Add components folder with general info component

---
+++ components/General-Info.jsx
@@ -0,0 +1,22 @@
+function Name(){
+    return
+
+}
+
+function Email(){
+    return
+
+}
+
+function Phone(){
+    return
+
+}
+
+export default function GeneralInfo() {
+    return {
+        <Name />
+        <Email />
+        <Phone />
+    }
+}
