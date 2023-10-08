4b69c1bc1eac1c591227865e15cd88dbc5ce5f05
Greg Johnston <gregmj.sco@gmail.com>
2023-10-08 11:21:15 +0900

    Combine generalInfo section functions into one to make form controls easier to work with

--- components/General-Info.jsx
+++ components/General-Info.jsx
@@ -1,49 +1,28 @@
 import "./General-Info.css"
 import { useState } from 'react';
 
-function Name(props){
-    const [nameText, updateNameText] = useState("");
+
+
+export default function GeneralInfo() {
+    const [nameText, setNameText] = useState("");
+
+    
     return (
-        <>
+        <form>
             <label htmlFor="name">
                 Name:
             </label>
-            <input type="text" id="name" name="name" placeholder="John Smith..."/>
-        </>
-    );
+            <input type="text" value={nameText} id="name" name="name" placeholder="John Smith..." required onChange={(e) => setNameText(e.target.value)} />
 
-}
-
-function Email(props){
-    return (
-        <>
             <label htmlFor="email">
                 Email:
             </label>
             <input type="email" name="email" id="email" placeholder="yourEmail@example.com"/>
-        </>
-    );
 
-}
-
-function Phone(props){
-    return (
-        <>
             <label htmlFor="phone">
                 Phone:
             </label>
             <input type="tel" name="phone" id="phone" placeholder="01-2345-6789"/>
-        </>
-    );
-
-}
-
-export default function GeneralInfo() {
-    return (
-        <form>
-            <Name />
-            <Email />
-            <Phone />
         </form>
     );
 }
