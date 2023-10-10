5de4d2d6bacc8f9906e0dba1cbb249fec2f3e278
Greg Johnston <gregmj.sco@gmail.com>
2023-10-10 10:53:31 +0900

    Add header component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import GeneralInfo from '../components/General-Info'
 import Education from '../components/Education'
+import Header from '../components/Header'
 
 function App() {
 
   return (
     <>
+      <Header />
       <GeneralInfo />
       <Education />
     </>
