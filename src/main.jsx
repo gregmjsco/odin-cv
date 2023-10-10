4848a53e9a5f91457b51ea220f7aeddbdefd81e0
Greg Johnston <gregmj.sco@gmail.com>
2023-10-10 10:50:23 +0900

    Reorganize components into App component and reset App css

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GeneralInfo />
-    <Education />
+    <App />
   </React.StrictMode>,
 )
