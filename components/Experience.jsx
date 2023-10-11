6585ed5adb015cfad5720df280864549b172dd05
Greg Johnston <gregmj.sco@gmail.com>
2023-10-11 18:44:07 +0900

    Add form field to experience section

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -3,6 +3,10 @@ export default function Experience() {
         <>
             <form action="">
                 <label htmlFor="job-title">Job Title</label>
+                <input type="text" id="job-title" />
+
+                <label htmlFor="company-name">Company Name</label>
+                <input type="text" id="company-name" />
             </form>
         </>
     )
